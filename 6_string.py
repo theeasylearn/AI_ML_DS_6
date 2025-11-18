4657c959981f48a5e6e123879f62180c00d4c63b
theeasylearn <theeasylearn@gmail.com>
2025-11-18 09:30:58 +0530

    string

---
+++ 6_string.py
@@ -0,0 +1,17 @@
+#create string in python 
+word = "THE_EASYLEARN_ACADEMY"
+print(word) #THE_EASYLEARN_ACADEMY
+print(word[0]) #T
+print(word[1]) #H
+print(word[3]) #_
+print(word[4:]) #EASYLEARN_ACADEMY
+print(word[4:13]) #EASYLEARN
+print(word * 3 ) #THE_EASYLEARN_ACADEMY THE_EASYLEARN_ACADEMY 
+print("welcome to " + word) # welcome to THE_EASYLEARN_ACADEMY
+word = 'T.E.L'
+print(word)
+# word[0] = None #error
+age = 21
+print("age = ", age) 
+print("age = " + str(age)) 
+print("Good bye")
