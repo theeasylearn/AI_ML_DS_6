3fe53b74b94798bb934d2e8ece926731fcd4f7b1
theeasylearn <theeasylearn@gmail.com>
2025-11-22 08:22:28 +0530

    operators in python

---
+++ 14_arithimatic.py
@@ -0,0 +1,28 @@
+# example of arithimatic operators
+num1 = int(input("Enter value for num1"))
+num2 = int(input("Enter value for num2"))
+
+print(f"num1 = {num1} num2 = {num2}")
+
+result = num1 + num2 
+print(f"addition = {result}")
+
+result = num1 - num2 
+print(f"subtraction = {result}")
+
+result = num1 * num2 
+print(f"multiplication = {result}")
+
+result = num1 / num2 
+print(f"division = {result}")
+
+result = num1 % num2 
+print(f"modulas = {result}")
+
+result = num1 // num2 
+print(f"floor division = {result}")
+
+result = num1 ** num2 
+print(f"exponent = {result}")
+
+
